8bfe75e0d9112fa05ecaf17fc0d9b91e20fc0cdb
Wisnu Agung Pambudi <wisnu.agung.001@gmail.com>
2023-11-17 10:38:27 +0700

    init commit

---
+++ src/main/java/kelompok_empat/entity/User.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kelompok_empat.entity;
+
+/**
+ *
+ * @author wisnu
+ */
+public class User {
+    private String nama;
+    private String email;
+    private String password;
+    private String noWa;
+
+    public User(String nama, String email, String password, String noWa) {
+        this.nama = nama;
+        this.email = email;
+        this.password = password;
+        this.noWa = noWa;
+    }
+}
